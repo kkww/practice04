0b75c4c284f60f5ca75edec7686f1aac2632a81f
kkww <kkww@kkww>
2017-05-01 14:01:11 +0900

    Practice 04) 5 update

--- src/prob05/MyBase.java
+++ src/prob05/MyBase.java
@@ -4,11 +4,20 @@ public class MyBase extends Base {
 	@Override
 	public void service(String state) {
 		if(state.equals("오후")) {
-			System.out.println("오후도 찾과 마찬가지로 일해야 합니다.");
+			day();
 		} else if(state.equals("낮")) {
-			System.out.println("낮에는 열심히 일하자!");
+			afternoon();
 		} else {
 			super.service(state);
 		}
+	}
+	
+	@Override
+	public void day() {
+		System.out.println("낮에는 열심히 일하자!");
+	}
+	
+	public void afternoon() {
+		System.out.println("오후도 찾과 마찬가지로 일해야 합니다.");
 	}
 }
